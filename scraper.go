64da8971bffdceca305efab739468738dd147483
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-11-18 21:01:18 +0500

    feat: Create parallel mode parser

--- scraper.go
+++ scraper.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"github.com/gocolly/colly" 
-	"encoding/csv" 
-	"log" 
-	"os" 
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/gocolly/colly"
 )
 
 type ShopProduct struct {
@@ -18,31 +20,13 @@ func main() {
 	var shopProducts []ShopProduct
 	var pagesToScrape []string
 
-	pageToScrape := "https://scrapeme.live/shop/page/1/" 
-	pagesDiscovered := []string{ pageToScrape }
-	i := 1
-	limit := 5
+	for i := 1; i <= 48; i++ {
+		pagesToScrape = append(pagesToScrape, "https://scrapeme.live/shop/page/"+strconv.Itoa(i)+"/")
+	}
 
-	c := colly.NewCollector()
-
+	c := colly.NewCollector(colly.Async(true))
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; rv:113.0) Gecko/20100101 Firefox/113.0"
-
-	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
-		newPaginationLink := e.Attr("href")
-
-		/*if !contains(pagesToScrape, newPaginationLink) {
-			if !contains(pagesDiscovered, newPaginationLink) {
-				pagesToScrape = append(pagesToScrape, newPaginationLink)
-			}
-			pagesDiscovered = append(pagesDiscovered, newPaginationLink)
-		}*/
-
-		if !contains(pagesDiscovered, newPaginationLink) {
-			pagesToScrape = append(pagesToScrape, newPaginationLink)
-			pagesDiscovered = append(pagesDiscovered, newPaginationLink)
-		}
-			
-	})
+	c.Limit(&colly.LimitRule{Parallelism: 4})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting: ", r.URL)
@@ -51,14 +35,14 @@ func main() {
 	c.OnError(func(_ *colly.Response, err error) {
 		fmt.Println("Something went wrong: ", err)
 	})
-	
+
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited: ", r.Request.URL)
 	})
 
 	c.OnHTML("li.product", func(e *colly.HTMLElement) {
 		shopProduct := ShopProduct{}
-		
+
 		shopProduct.url = e.ChildAttr("a", "href")
 		shopProduct.image = e.ChildAttr("img", "src")
 		shopProduct.name = e.ChildText("h2")
@@ -67,18 +51,11 @@ func main() {
 		shopProducts = append(shopProducts, shopProduct)
 	})
 
-	c.OnScraped(func(response *colly.Response) {
-		if len(pagesToScrape) != 0 && i < limit {
-			pageToScrape = pagesToScrape[0]
-			pagesToScrape = pagesToScrape[1:]
+	for _, pageToScrape := range pagesToScrape {
+		c.Visit(pageToScrape)
+	}
 
-			i++
-
-			c.Visit(pageToScrape)
-		}
-	})
-	
-	c.Visit(pageToScrape)
+	c.Wait()
 
 	file, err := os.Create("export/products.csv")
 	if err != nil {
@@ -104,10 +81,9 @@ func main() {
 			shopProduct.name,
 			shopProduct.price,
 		}
-
 		writer.Write(record)
 	}
-	
+
 	defer writer.Flush()
 
 	fmt.Printf("%v", shopProducts)
@@ -119,6 +95,5 @@ func contains(s []string, str string) bool {
 			return true
 		}
 	}
-
 	return false
 }
